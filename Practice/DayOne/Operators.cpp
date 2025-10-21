e064e70e7fb3f406a757a71187e2b1d425097d49
Mihretf <mihretworku94@gmail.com>
2025-10-21 15:16:56 +0300

    practice

---
+++ Practice/DayOne/Operators.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a= 5, b= 100;
+
+    int x= 10, y= 5;
+
+
+    cout << "a+b= " << a+b << endl;
+    cout << "a-b= " << a-b <<endl;
+    cout << "a*b= " << a*b<< endl;
+    cout << "a/b= " << a/b <<endl;
+    cout << "a % b= " << a%b << endl;
+
+
+    cout << (x>y) <<endl;
+    cout << (x<y) <<endl;
+    cout << (x==y) <<endl;
+    cout << (x!=y) <<endl;
+
+
+    return 0;
+}
