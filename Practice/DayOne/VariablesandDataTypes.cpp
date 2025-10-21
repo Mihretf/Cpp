4a360ddeef2e5f6ca701e1ac702e33587014b81b
Mihretf <mihretworku94@gmail.com>
2025-10-21 14:11:55 +0300

    practice

---
+++ Practice/DayOne/VariablesandDataTypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age = 20;
+    double weight = 2.39;
+    char grade = 'A';
+    string name = "Mihret";
+    bool isStudent = true;
+
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Weight: " << weight << endl;
+    cout << "Is student: " << isStudent << endl;
+
+    return 0;
+
+}
