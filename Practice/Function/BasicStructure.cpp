2e74d117d57d4816731be65939d114bcb0df14fd
Mihretf <mihretworku94@gmail.com>
2025-10-29 10:47:53 +0300

    basic function structure

---
+++ Practice/Function/BasicStructure.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+void greet (){ // void means the function does not return a value
+    cout << "Hello, Mihret!" <<endl;
+}
+
+int main() {
+    greet();
+    return 0;
+}
